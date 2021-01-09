1132e59e43867c75b50d58c220b5aca89800a0aa
Grayson <grayyday@gmail.com>
2021-01-08 19:14:59 -0700

    Backed up computer and wanted to make sure everything on git was current

--- app.js
+++ app.js
@@ -1,39 +1,54 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js");
 
 const app = express();
 
-var items = ["Buy Food", "Cook Food", "Eat Food"];
+let items = ["Buy Food", "Cook Food", "Eat Food"];
+let workItems = [];
 
 app.set("view engine", "ejs");
 
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
 
 app.get("/", function(req, res) {
 
-  var today = new Date();
-
-var options = {
-  weekday: "long",
-  day: "numeric",
-  month: "long"
-};
-
-  var day = today.toLocaleDateString("en-US", options);
+let day = date();
 
   res.render("list", {
-    kindOfDay: day,
+    listTitle: day,
     newListItems: items
   });
 
   app.post("/", function(req, res){
-    var item = req.body.newItem;
 
-    items.push(item);
+    let item = req.body.newItem;
 
-    res.redirect("/");
+    if (req.body.list === "Work"){
+      workItems.push(item);
+      res.redirect("/work");
+    } else{
+      items.push(item);
+      res.redirect("/");
+    }
+
   });
 
+});
+
+app.get("/work", function(req, res){
+  res.render("list", {listTitle: "Work List", newListItems: workItems});
+});
+
+app.post("/work", function(req, res){
+  let item = req.body.newItem;
+  workItems.push(item);
+  res.redirect("/work");
+});
+
+app.get("/about", function(req, res){
+  res.render("about");
 });
 
 
